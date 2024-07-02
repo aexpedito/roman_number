b6d6ce93a7d879d592a775a4f1827eaf7f4414ab
aexpedito <aexpedito@hotmail.com>
2024-07-01 21:48:59 -0300

    Added tests

--- roman_tests.py
+++ roman_tests.py
@@ -10,9 +10,17 @@ class RomanTestCases(unittest.TestCase):
         pass
     def tearDown(self):
         pass
+    #caracter invalido
     def test_roman_to_int_test_invalid_char(self):
         self.assertEqual(roman_to_int("XIB"), -1)
+    #caracters validos
+    def test_roman_to_int_test_invalid_char_40(self):
+        self.assertEqual(roman_to_int("XL"), 40)
 
+    #mais de 3 caracteres iguais e consecutivos
+    def test_roman_to_int_test_char_count_regex(self):
+        self.assertEqual(roman_to_int("XXXX"), -1)
+    #um unico caracter
     def test_roman_to_int_test_I(self):
         self.assertEqual(roman_to_int("I"), 1)
     def test_roman_to_int_test_V(self):
@@ -28,32 +36,26 @@ class RomanTestCases(unittest.TestCase):
     def test_roman_to_int_test_M(self):
         self.assertEqual(roman_to_int("M"), 1000)
 
-    def test_roman_to_int_test_char_count_regex(self):
-        self.assertEqual(roman_to_int("XXXX"), -1)
-
-    def test_roman_to_int_test_size1_1(self):
-        self.assertEqual(roman_to_int("I"), 1)
-
-    def test_roman_to_int_test_size1_10(self):
-        self.assertEqual(roman_to_int("X"), 10)
-
+    #2 caracteres e 1 subtracao
     def test_roman_to_int_test_size2_4(self):
         self.assertEqual(roman_to_int("IV"), 4)
-
     def test_roman_to_int_test_size2_2(self):
         self.assertEqual(roman_to_int("II"), 2)
 
-    def test_roman_to_int_test_size3_3(self):
-        self.assertEqual(roman_to_int("III"), 3)
-
-    def test_roman_to_int_test_size2_2(self):
-        self.assertEqual(roman_to_int("II"), 2)
-
-    def test_roman_to_int_test_size5_18(self):
-        self.assertEqual(roman_to_int("XVIII"), 18)
-
-    def test_roman_to_int_test_size5_19(self):
+    #3 caracteres e 1 subtracao
+    def test_roman_to_int_test_size3_19(self):
         self.assertEqual(roman_to_int("XIX"), 19)
+    def test_roman_to_int_test_size3_30(self):
+        self.assertEqual(roman_to_int("XXX"), 30)
 
-    def test_roman_to_int_test_size9_88(self):
-        self.assertEqual(roman_to_int("LXXXVIII"), 88)
+    #4 caracteres com 1 subtração
+    def test_roman_to_int_test_size4_24(self):
+        self.assertEqual(roman_to_int("XXIV"), 24)
+    def test_roman_to_int_test_size4_26(self):
+        self.assertEqual(roman_to_int("XXVI"), 26)
+
+    #4 caracteres com 2 subtraçoes
+    def test_roman_to_int_test_size4_490(self):
+        self.assertEqual(roman_to_int("CDXC"), 490)
+    def test_roman_to_int_test_size4_29(self):
+        self.assertEqual(roman_to_int("XXIX"), 29)
